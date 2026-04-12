1970b796d788de7e98e4077431a68cde84c648ff
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-12 14:42:42 +0200

    Add basic request handling for ping and time messages

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,3 +1,21 @@
+import java.util.Date;
+
 public class ClientHandler {
-    
+
+    public static String handleRequest(String message, String clientAddress) {
+
+      
+    if (message.equals("ping")) return "pong";
+
+
+        if (message.equalsIgnoreCase("ping")) {
+            return "pong";
+        } 
+        else if (message.equalsIgnoreCase("time")) {
+            return new Date().toString();
+        } 
+        else {
+            return "Mesazh i pranuar: " + message;
+        }
+    }
 }
