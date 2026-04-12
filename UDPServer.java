9aaa9821633e55a7f01ec42879b31a543d18bc38
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-12 07:29:39 +0200

    Implementimi i Serverit UDP dhe kufizimi i lidhjeve te klienteve

--- UDPServer.java
+++ UDPServer.java
@@ -1,20 +1,62 @@
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.util.HashSet;
+import java.util.Set;
 
-public class UDPServer{
+public class UDPServer {
 
     private static final String SERVER_IP = "127.0.0.1";
     private static final int PORT = 5000;
+    private static final int MAX_CLIENTS = 3;
+
+  
+    private static Set<String> clients = new HashSet<>();
 
     public static void main(String[] args) {
-        try{
+        try {
             DatagramSocket socket = new DatagramSocket(PORT);
-            InetAddress ip = InetAddress.getByName(SERVER_IP);
 
-            System.out.println("Serveri u startua!");
-            System.out.println("IP:" + ip.getHostAddress());
-            System.out.print("Port: " + PORT);
-        } catch (Exception e){
+            System.out.println("Serveri po dëgjon në portin: " + PORT);
+
+            while (true) {
+                byte[] buffer = new byte[1024];
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+                socket.receive(packet);
+
+                String clientAddress = packet.getAddress().getHostAddress() + ":" + packet.getPort();
+
+                if (!clients.contains(clientAddress)) {
+
+                    
+                    if (clients.size() >= MAX_CLIENTS) {
+                        String response = "Serveri i mbingarkuar. Provo më vonë!";
+                        byte[] responseData = response.getBytes();
+
+                        DatagramPacket responsePacket = new DatagramPacket(
+                                responseData,
+                                responseData.length,
+                                packet.getAddress(),
+                                packet.getPort()
+                        );
+
+                        socket.send(responsePacket);
+                        System.out.println("Klienti u refuzua: " + clientAddress);
+                        continue;
+                    }
+
+
+                    clients.add(clientAddress);
+                    System.out.println("Klient i ri u lidh: " + clientAddress);
+                }
+
+               
+                String message = new String(packet.getData(), 0, packet.getLength());
+                System.out.println("Mesazh nga " + clientAddress + ": " + message);
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
