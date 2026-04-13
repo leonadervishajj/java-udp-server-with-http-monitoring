25f6ba7b66ce768c8361b98d7e0874082bdbe92c
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-13 12:44:49 +0200

    Inicializimi i serverit UDP dhe kontrolli i pragut të lidhjeve

--- ClientManager.java
+++
@@ -1,3 +0,0 @@
-public r ClientManager {
-    
-}
--- UDPServer.java
+++ UDPServer.java
@@ -1,34 +1,37 @@
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class UDPServer {
-    
     private static final String SERVER_IP = "127.0.0.1";
     private static final int PORT = 5000;
+    private static final int BUFFER_SIZE = 1024;
 
     public static void main(String[] args) {
         try {
-           
+       
             DatagramSocket socket = new DatagramSocket(PORT);
             InetAddress ip = InetAddress.getByName(SERVER_IP);
+            
+            System.out.println("Serveri UDP startoi ne " + SERVER_IP + ":" + PORT);
 
-            System.out.println("Serveri u startua!");
-            System.out.println("IP: " + ip.getHostAddress());
-            System.out.println("Port: " + PORT);
-
-         
-            byte[] buffer = new byte[1024];
-            System.out.println("Duke dëgjuar për klientët...");
+            byte[] receiveData = new byte[BUFFER_SIZE];
 
             while (true) {
-            
-                java.net.DatagramPacket packet = new java.net.DatagramPacket(buffer, buffer.length);
-                socket.receive(packet); 
-                
-                String mesazhi = new String(packet.getData(), 0, packet.getLength());
-                System.out.println("U pranua: " + mesazhi);
-            }
+        
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
 
+                String clientAddress = receivePacket.getAddress().getHostAddress() + ":" + receivePacket.getPort();
+                
+                if (ClientManager.registerClient(clientAddress)) {
+                    System.out.println("Lidhje e re e pranuar: " + clientAddress);
+                } else {
+                    System.out.println("Lidhja u refuzua per: " + clientAddress + " (Limiti u arrit)");
+                }
+
+                receiveData = new byte[BUFFER_SIZE];
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
