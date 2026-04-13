406530ea622157854bc55c05e8597408e47f19b1
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-13 18:58:06 +0200

    Integrimi i MessageLogger ne Server

--- UDPServer.java
+++ UDPServer.java
@@ -2,50 +2,39 @@ import java.net.*;
 import java.io.*;
 
 public class UDPServer {
-    private static final int PORT = 1234; // Porta ku dëgjon serveri
+    private static final int PORT = 1234;
+    private static final String IP_ADDRESS = "localhost";
 
     public static void main(String[] args) {
+        
         try (DatagramSocket socket = new DatagramSocket(PORT)) {
-            System.out.println("UDP Serveri u nis në portën " + PORT + "...");
-            System.out.println("Duke pritur për mesazhe...");
+            System.out.println("🚀 Serveri UDP nisi ne " + IP_ADDRESS + ":" + PORT);
 
-            byte[] receiveBuffer = new byte[1024];
+            byte[] buffer = new byte[1024];
 
             while (true) {
-                // 1. Pritja e paketës nga klienti (Pika 1)
-                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
                 socket.receive(receivePacket);
-
+                String clientAddress = receivePacket.getAddress().toString();
                 String message = new String(receivePacket.getData(), 0, receivePacket.getLength()).trim();
-                String clientAddress = receivePacket.getAddress().getHostAddress() + ":" + receivePacket.getPort();
 
-               
-                if (ClientManager.registerClient(clientAddress)) {
-                    System.out.println(" Mesazhi i pranuar nga [" + clientAddress + "]: " + message);
+                System.out.println("Mesazh i ri nga " + clientAddress + ": " + message);
+                MessageLogger.log(clientAddress, message);
 
-                 
-                    String ackMessage = "ACK: Mesazhi u pranua me sukses!";
-                    byte[] sendData = ackMessage.getBytes();
+                String responseMessage = "ACK: Mesazhi u pranua dhe u ruajt ne log.";
+                byte[] sendData = responseMessage.getBytes();
 
-                   
-                    DatagramPacket sendPacket = new DatagramPacket(
-                        sendData, 
-                        sendData.length, 
-                        receivePacket.getAddress(), 
-                        receivePacket.getPort()
-                    );
-
-                    socket.send(sendPacket);
-                    System.out.println("📤 ACK u dërgua te klienti.");
-
-                
-                } else {
-                  
-                    System.out.println("Lidhja u refuzua për: " + clientAddress);
-                }
+                DatagramPacket sendPacket = new DatagramPacket(
+                    sendData, 
+                    sendData.length, 
+                    receivePacket.getAddress(), 
+                    receivePacket.getPort()
+                );
+                socket.send(sendPacket);
             }
         } catch (Exception e) {
-            System.err.println("Gabim në Server: " + e.getMessage());
+            System.err.println(" Gabim ne Server: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
