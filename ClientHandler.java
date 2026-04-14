2944b4ee087adeb06f3bc35b684e2319f98a724f
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-14 16:12:36 +0200

    - Implementimi i klasës ClientHandler për procesimin e kërkesave.

--- ClientHandler.java
+++ ClientHandler.java
@@ -2,20 +2,22 @@ import java.io.File;
 import java.util.Arrays;
 
 public class ClientHandler {
+
     public static String handleRequest(String message) {
         String request = message.trim().toUpperCase();
-
         if (request.equals("LISTA")) {
+           
             File folder = new File("."); 
             String[] files = folder.list();
-            
-            if (files != null) {
-                return "Skedarët në server: " + Arrays.toString(files);
+
+            if (files != null && files.length > 0) {
+                return "Skedaret ne Server: " + Arrays.toString(files);
             } else {
-                return " Nuk u gjet asnjë skedar.";
+                return "Serveri nuk ka asnje skedar per te treguar.";
             }
         }
+
         
-        return "ACK: Mesazhi u pranua.";
+        return "ACK: Mesazhi u pranua, por nuk u kerka lista e skedareve.";
     }
 }
