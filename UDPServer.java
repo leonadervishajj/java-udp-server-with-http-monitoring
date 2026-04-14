b9d24df2107a444195feb0941d9e068121b1cdb5
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-14 23:28:09 +0200

    Finalizimi i UDPServer me funksionalitetet e plota

--- UDPServer.java
+++ UDPServer.java
@@ -2,42 +2,48 @@ import java.net.*;
 import java.io.*;
 
 public class UDPServer {
-  
     private static final int PORT = 1234;
-    private static final String IP_ADDRESS = "localhost";
 
     public static void main(String[] args) {
-        try (DatagramSocket socket = new DatagramSocket(PORT)) {
-            System.out.println("Serveri UDP nisi në " + IP_ADDRESS + ":" + PORT);
+        // Kjo lejon monitorimin në browser (http://localhost:8080/stats)
+        new Thread(new HttpServer()).start();
 
-            byte[] buffer = new byte[1024];
+        try (DatagramSocket socket = new DatagramSocket(PORT)) {
+            System.out.println("UDP Serveri nisi ne portin " + PORT);
+            System.out.println(" Pika 6: Shkruaj 'LISTA' te klienti per te pare fajllat.");
+
+            byte[] buffer = new byte[2048];
 
             while (true) {
                 DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
                 socket.receive(receivePacket);
 
-                String clientIP = receivePacket.getAddress().toString() + ":" + receivePacket.getPort();
+            
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+                String clientID = clientAddress.toString() + ":" + clientPort;
+
+
                 String message = new String(receivePacket.getData(), 0, receivePacket.getLength()).trim();
+                System.out.println("Mesazh nga [" + clientID + "]: " + message);
+                MessageLogger.log(clientID, message);
 
-                System.out.println("Mesazh nga " + clientIP + ": " + message);
-
-              
-                MessageLogger.log(clientIP, message);
-                TimeoutManager.updateActivity(clientIP);
-                
+       
+                TimeoutManager.updateActivity(clientID);
                 TimeoutManager.checkTimeouts();
-                String responseMessage = "ACK: Mesazhi u mor. Statusi: Aktiv";
-                byte[] sendData = responseMessage.getBytes();
+                String responseMessage = ClientHandler.handleRequest(message);
 
+                
+                byte[] sendData = responseMessage.getBytes();
                 DatagramPacket sendPacket = new DatagramPacket(
                     sendData, sendData.length, 
-                    receivePacket.getAddress(), 
-                    receivePacket.getPort()
+                    clientAddress, 
+                    clientPort
                 );
                 socket.send(sendPacket);
             }
         } catch (Exception e) {
-            System.err.println("Gabim: " + e.getMessage());
+            System.err.println(" Gabim ne UDPServer: " + e.getMessage());
             e.printStackTrace();
         }
     }
