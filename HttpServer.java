120498073cc17b5963cb30f9bba764ba35724875
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-14 20:21:15 +0200

    Krijimi i klasës HttpServer që implementon Runnable.

--- HttpServer.java
+++ HttpServer.java
@@ -7,12 +7,10 @@ public class HttpServer implements Runnable {
     @Override
     public void run() {
         try {
-
             com.sun.net.httpserver.HttpServer server = com.sun.net.httpserver.HttpServer.create(new InetSocketAddress(8080), 0);
-            
             server.createContext("/stats", (exchange) -> {
                 String response = "<html><body>" +
-                                  "<h1>📊 UDP Server Monitoring</h1>" +
+                                  "<h1>UDP Server Monitoring</h1>" +
                                   "<p><b>Statusi:</b> ONLINE</p>" +
                                   "<p><b>Porti UDP:</b> 1234</p>" +
                                   "<p><b>Porti HTTP:</b> 8080</p>" +
@@ -29,10 +27,10 @@ public class HttpServer implements Runnable {
 
             server.setExecutor(null); // Perdor ekzekutuesin default
             server.start();
-            System.out.println(" HTTP Serveri nisi ne portin 8080 (Shiko: http://localhost:8080/stats)");
+            System.out.println("HTTP Serveri nisi ne portin 8080 (Shiko: http://localhost:8080/stats)");
             
         } catch (IOException e) {
-            System.err.println("Gabim ne HTTP Server: " + e.getMessage());
+            System.err.println(" Gabim ne HTTP Server: " + e.getMessage());
         }
     }
 }
